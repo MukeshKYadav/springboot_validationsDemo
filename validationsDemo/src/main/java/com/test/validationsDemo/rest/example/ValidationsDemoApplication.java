d463d6450837f38536337822082aef0145b84c9c
MukeshKYadav <mukesh.boomi@gmail.com>
2019-04-26 15:40:09 +0530

    test changes

---
+++ validationsDemo/src/main/java/com/test/validationsDemo/rest/example/ValidationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.validationsDemo.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationsDemoApplication.class, args);
+	}
+
+}
---
+++ validationsDemo/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values(10001,'Mukesh', 'M1234567');
+
+insert into student
+values(10002,'Ravi', 'R2345678');
